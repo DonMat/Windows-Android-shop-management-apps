4f9e081507e3c1ab816adcf26e588cc1861e5844
MateuszU <donmat@outlook.com>
2014-06-07 18:28:41 +0200

    Werfikowanie poprawnosci wprowadzonego tekstu. Dozwolone sa tylko znaki alfanumeryczne.

--- Android/src/com/io/sklep/klientapp/LoginForm.java
+++ Android/src/com/io/sklep/klientapp/LoginForm.java
@@ -1,15 +1,21 @@
 package com.io.sklep.klientapp;
 
+
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginForm extends Fragment {
 
-    public static final String IMAGE_RESOURCE_ID = "iconResourceID";
-    public static final String ITEM_NAME = "itemName";
+    public Button button;
+    public EditText edit;
+    final String match = "[A-Za-z\\d]+";
 
     public LoginForm() {
 
@@ -21,7 +27,35 @@ public class LoginForm extends Fragment {
 
           View view = inflater.inflate(R.layout.login_form, container,
                       false);
-
+          button = (Button) view.findViewById(R.id.bLogi);
+          edit = (EditText) view.findViewById(R.id.eLogin);
+          
+          edit.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				// TODO Auto-generated method stub
+				
+				String a = edit.getText().toString();
+				if(! a.matches(match))
+					button.setEnabled(false);
+				else
+					button.setEnabled(true);
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
           return view;
     }
 
